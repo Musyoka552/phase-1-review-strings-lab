72fd9e3bb99e851df8e651b7f1282cdd6cee084b
Musyoka552 <kilonzopatoh6@gmail.com>
2024-07-23 04:54:46 +0300

    strings

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Write your code in this file!
-const currentUser = 'Patrick Musyoka';
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
-const excitedWelcomeMessage = welcomeMessage.toUpperCase()
-currentUser.slice(0, 1);
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+//const currentUser = 'Grace Hopper';
+//const welcomeMessage = 'Welcome to Flatbook, currentUser';
+const currentUser = 'Grace Hopper';
 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
